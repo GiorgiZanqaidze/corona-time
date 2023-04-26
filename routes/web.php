d91677ee34f74661860add5f035de86eaeea3f8e
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-26 21:25:12 +0400

    refactor: group routes in middlewares

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,28 +22,32 @@ Route::controller(AuthController::class)->group(function () {
 	Route::post('logout', 'logout')->name('logout')->middleware(['auth', 'verify_email']);
 });
 
-Route::controller(DashboardController::class)->group(function () {
-	Route::get('landing-worldwide', 'worldwide')->name('worldwide')->middleware('verify_email');
-	Route::get('landing-bycountry', 'byCountry')->name('by-country')->middleware('verify_email');
+Route::middleware(['auth', 'verify_email'])->group(function () {
+	Route::controller(DashboardController::class)->group(function () {
+		Route::get('landing-worldwide', 'worldwide')->name('worldwide');
+		Route::get('landing-bycountry', 'byCountry')->name('by-country');
+	});
 });
 
-Route::controller(DashboardController::class)->group(function () {
-	Route::post('register', 'postRegistration')->middleware('guest')->name('register.store');
-	Route::get('account/verify/{token}', 'verifyAccount')->name('user.verify');
-});
+Route::middleware('guest')->group(function () {
+	Route::controller(RegisterController::class)->group(function () {
+		Route::post('register', 'postRegistration')->name('register.store');
+		Route::get('account/verify/{token}', 'verifyAccount')->name('user.verify');
+	});
 
-Route::controller(DashboardController::class)->group(function () {
-	Route::post('reset-password', 'postPassword')->middleware('guest')->name('reset-password');
-	Route::get('account/verify/password/{token}', 'edit')->middleware('guest')->name('user.update');
-	Route::patch('account/verify/password/{token}', 'update')->middleware('guest')->name('user.edit');
-});
+	Route::controller(ResetPasswordController::class)->group(function () {
+		Route::post('reset-password', 'postPassword')->name('reset-password');
+		Route::get('account/verify/password/{token}', 'edit')->name('user.update');
+		Route::patch('account/verify/password/{token}', 'update')->name('user.edit');
+	});
 
-Route::get('/', function () { return view('login'); })->middleware('guest')->name('login');
-Route::get('register', function () { return view('register'); })->middleware('guest')->name('register');
-Route::get('show-email', function () { return view('show-email'); })->middleware('auth')->name('show-email');
-Route::get('set-new-password', function () { return view('set-new-password'); })->middleware('guest')->name('set-new-password');
-Route::get('reset-password', function () { return view('reset-password'); })->middleware('guest')->name('reset-password');
-Route::get('confirmation-password', function () { return view('confirmation-password'); })->middleware('guest')->name('confirmation-password');
+	Route::get('/', function () { return view('login'); })->name('login');
+	Route::get('register', function () { return view('register'); })->name('register');
+	Route::get('show-email', function () { return view('show-email'); })->name('show-email');
+	Route::get('set-new-password', function () { return view('set-new-password'); })->name('set-new-password');
+	Route::get('reset-password', function () { return view('reset-password'); })->name('reset-password');
+	Route::get('confirmation-password', function () { return view('confirmation-password'); })->name('confirmation-password');
+});
 
 // language route
 Route::get('lang/{lang}', [LanguageController::class, 'switchLang'])->name('lang.switch');
