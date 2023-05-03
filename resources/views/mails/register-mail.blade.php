7131c356bda8912bea2ebfd6b050b0f0e1774a75
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-05-03 12:18:33 +0400

    feat: change mail inline style

--- resources/views/mails/register-mail.blade.php
+++ resources/views/mails/register-mail.blade.php
@@ -43,12 +43,37 @@
             box-sizing: border-box;
         }
     </style>
-    <div class="flex">
-        <div class="center-div">
-            <h1>Confirmation email</h1>
-            <p>click this button to verify your email</p>
-            <div class="verify">
-                <a href="{{ route('user.verify', $token) }}">VERIFY EMAIL</a>
+    <div class="flex" style="margin: 0 auto;
+    flex-direction: column;
+    gap: 10px;
+    font-family: 'Inter';">
+        <div class="center-div" style="display: block;
+        margin: 0 auto;
+        width: 400px;
+        padding-bottom: 0;
+        margin-bottom: 0;">
+            <h1 style="padding: 1rem;
+            color: white;
+            border-radius: 8px;
+            width: 100%;
+            text-align: center;">Confirmation email</h1>
+            <p style="padding: 1rem;
+            color: white;
+            border-radius: 8px;
+            width: 100%;
+            text-align: center;">click this button to verify your email</p>
+            <div class="verify" style="width: 100%;
+            margin-left: 20px;
+            margin-right: 20px;
+            box-sizing: border-box;">
+                <a href="{{ route('user.verify', $token) }}" style="display: inline-block;
+                background-color: #0FBA68;
+                text-decoration: none;
+                padding: 1rem;
+                color: white;
+                border-radius: 8px;
+                width: 100%;
+                text-align: center;">VERIFY EMAIL</a>
             </div>
         </div>
     </div>
