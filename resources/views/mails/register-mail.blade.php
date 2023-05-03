19b47b8d7cbafc8799d62b1855292308b3d05828
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-05-03 12:19:26 +0400

    chore: style

--- resources/views/mails/register-mail.blade.php
+++ resources/views/mails/register-mail.blade.php
@@ -7,42 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <style>
-        .flex {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            gap: 10px;
-            font-family: 'Inter';
-        }
-        .center-div {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            gap: 10px;
-            width: 400px;
-            padding-bottom: 0;
-            margin-bottom: 0;
-        }
-        a {
-            display: inline-block;
-            background-color: #0FBA68;
-            text-decoration: none;
-            padding: 1rem;
-            color: white;
-            border-radius: 8px;
-            width: 100%;
-            text-align: center;
-        }
-        .verify {
-            width: 100%;
-            margin-left: 20px;
-            margin-right: 20px;
-            box-sizing: border-box;
-        }
-    </style>
     <div class="flex" style="margin: 0 auto;
     flex-direction: column;
     gap: 10px;
